394143bf0fb6e1099794e70cc312e0d9fd9ff2a3
Andres <andresgr2094@hotmail.com>
2024-08-21 18:31:47 -0400

    adding scottire

--- nitro/src/components/Card.tsx
+++ nitro/src/components/Card.tsx
@@ -1,6 +1,6 @@
 import { useState} from 'react';
 import '../css/Card.css';
-import sample from '../sample-gif.gif';
+import sample from '../images/scottiev1.gif';
 
 interface Player{
     id: number;
@@ -31,7 +31,7 @@ export default function Card({player} : CardProps){
         <div className={`relative w-72 h-96 transition-transform duration-500 transform-style-preserve-3d ${isFlipped ? 'rotate-y-180' : ''}`}>
           <div className="absolute w-full h-full flex flex-col items-center justify-center bg-black backface-hidden rounded-lg p-4 border-4 border-silver shadow-md">
             <h2 className="text-white text-2xl">No. {player.number}</h2>
-            <img src={sample} alt="Bit art of player"/>
+            <img src={sample} alt="Bit art of player" className='w-full h-full image-rendering-pixelated image-rendering-crisp'/>
             <p className="text-white mt-2">{player.name}</p>
           </div>
           <div className="absolute w-full h-full grid grid-cols-2 bg-black backface-hidden rotate-y-180 rounded-lg p-4 border-4 border-silver shadow-md">
@@ -43,7 +43,7 @@ export default function Card({player} : CardProps){
                 <p>{player.quote}</p>
                 </div>
                 <div className='flex justify-center itemrs-center'>
-                <img src={sample} alt="Image of player" className='w-full mas-w-xs'/>
+                <img src={sample} alt="Image of player" className='w-full max-w-xs'/>
                 </div>
                 <div className="col-span-2 text-left">
                 <p className="mt-2">Likes: {player.likes}</p>
--- nitro/tailwind.config.js
+++ nitro/tailwind.config.js
@@ -7,7 +7,27 @@ module.exports = {
         nitro: "#8ACE00",
         silver: "C0C0C0",
       },
+      imageRendering: {
+        pixelated: {
+          "image-rendering": "pixelated",
+        },
+        "crisp-edges": {
+          "image-rendering": "crisp-edges",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".image-rendering-pixelated": {
+          "image-rendering": "pixelated",
+        },
+        ".image-rendering-crisp": {
+          "image-rendering": "crisp-edges",
+        },
+      };
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
